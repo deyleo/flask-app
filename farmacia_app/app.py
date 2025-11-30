ff2114f56cfa1152c5a40895dc2e5c9a93063a24
deyleo <deyvisl179@gmail.com>
2025-11-29 21:38:59 -0500

    ActualizacionNUEVA

--- farmacia_app/app.py
+++ farmacia_app/app.py
@@ -27,7 +27,7 @@ def get_db_connection():
     conn = pymysql.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
+        password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
         cursorclass=pymysql.cursors.DictCursor,
         autocommit=True
